8423bd29018ff7b0f419b06e0640d8c4e2791cb6
Gabriel Roldan <groldan@boundlessgeo.com>
2015-08-30 16:41:27 -0300

    HzLockProvider: Hazelcast implementation of org.geoserver.platform.resource.LockProvider.
    This patch adds distributed locking for geoserver resources through
    hz-cluster module's HzLockProvider. Needs to be chosen in the Global
    Configuration page to take effect.

---
+++ src/community/hz-cluster/src/main/java/org/geoserver/cluster/hazelcast/HzResourceListener.java
@@ -0,0 +1,18 @@
+/* (c) 2015 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.cluster.hazelcast;
+
+import org.geoserver.platform.resource.ResourceListener;
+import org.geoserver.platform.resource.ResourceNotification;
+
+public class HzResourceListener implements ResourceListener{
+
+    @Override
+    public void changed(ResourceNotification notify) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
